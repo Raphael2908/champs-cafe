fa6decd3c18f6b721a841f4c5351507a663a168b
Raphael Lim <raphael@Raphaels-MacBook-Pro.local>
2022-12-13 16:44:28 +0800

    fix: app header

--- components/Header.vue
+++ components/Header.vue
@@ -11,11 +11,13 @@
     
             <!-- Nav Items Small -->
             <ul v-show="navItems" class="md:hidden z-10 absolute flex flex-col gap-5 items-center inset-x-0 -bottom-52 bg-white rounded-md mx-5 py-2 shadow-lg border-grey-400 border md:static md:flex-row md:shadow-none md:bg-transparent md:border-0">
-                <li>
-                    <h1>Menu</h1>
+                 <li>
+                    <NuxtLink to="/menu">
+                        <Button class="bg-sky-400 text-white shadow-sky-400/50 shadow-lg">Menu</Button>
+                    </NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/menu">
+                    <NuxtLink to="/contactUs">
                         <Button class="bg-orange-400 text-white shadow-orange-400/50 shadow-lg">Contact Us</Button>
                     </NuxtLink>
                 </li>
